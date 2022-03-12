3afe516a4a2e4d6b0122a5e72062941ac76fb819
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2022-03-11 21:00:52 -0800

    Update submission.cpp

--- submission.cpp
+++ submission.cpp
@@ -1,17 +1,10 @@
-#include <bits/stdc++.h>
-#define fst first
-#define snd second
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define SZ(s) int(s.size())
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> ii; 
 
 int main()
 {
-    FIN;
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	return 0;
 }
